3ab31bad83cf4af0d19a1fa79f3002bc75fdf7a8
karimlik <okarimlikarim@gmail.com>
2023-03-25 15:23:36 +0400

    Rewrote CarListItemViewModel, still blank

--- E-Commerce/AppClient/ViewModels/CarListItemViewModel.cs
+++ E-Commerce/AppClient/ViewModels/CarListItemViewModel.cs
@@ -1,12 +1,11 @@
 ﻿using E_Commerce.Data.Models;
 using System;
+using System.ComponentModel;
 
 namespace AppClient.ViewModels
 {
     public class CarListItemViewModel
     {
-        public Car Car { get; set; }
-
         public string Make => Car.Make;
         public string Model => Car.Model;
         public int Year => Car.Year;
@@ -14,9 +13,36 @@ namespace AppClient.ViewModels
         public decimal Price => Car.Price;
         public string ImageUrl => Car.imgUrl;
 
+        private Car _car;
+        public Car Car
+        {
+            get { return _car; }
+            set
+            {
+                if (_car != value)
+                {
+                    _car = value;
+                    RaisePropertyChanged(nameof(Car));
+                    RaisePropertyChanged(nameof(Make));
+                    RaisePropertyChanged(nameof(Model));
+                    RaisePropertyChanged(nameof(Year));
+                    RaisePropertyChanged(nameof(Mileage));
+                    RaisePropertyChanged(nameof(Price));
+                    RaisePropertyChanged(nameof(ImageUrl));
+                }
+            }
+        }
+
         public CarListItemViewModel(Car car)
         {
             Car = car ?? throw new ArgumentNullException(nameof(car));
         }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
