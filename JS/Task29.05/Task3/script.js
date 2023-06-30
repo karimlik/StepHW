cf9fb0f0d0b1c465f3c8736853ef10b75496a997
Karim Karimli <123446675+karimlik@users.noreply.github.com>
2023-06-30 22:28:17 +0400

    Delete JS directory from ASP branch

--- JS/Task29.05/Task3/script.js
+++
@@ -1,9 +0,0 @@
-const container = document.getElementById('container');
-const ball = document.getElementById('ball');
-
-container.addEventListener('click', (event) => {
-  const posX = event.clientX - container.offsetWidth / 2;
-  const posY = event.clientY - container.offsetHeight / 2;
-
-  ball.style.transform = `translate(${posX}px, ${posY}px)`;
-});
