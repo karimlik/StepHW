2e1edebae9c8f80ee1993815fe5129bffa6a9c66
karimlik <okarimlikarim@gmail.com>
2023-07-26 12:50:40 +0300

    currency

---
+++ ASP/CurrencyConverter/Helpers/XmlDocumentHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using CurrencyConverter.Models;
+
+namespace CurrencyConverter.Helpers
+{
+    public class XmlDocumentHelper
+    {
+        public static List<Currency> FetchDataFromXml()
+        {
+            List<Currency> currencies = new List<Currency>();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("https://www.cbar.az/currencies/17.07.2023.xml");
+
+            XmlNodeList currencyNodes = xmlDoc.SelectNodes("//ValType/Valute");
+            foreach (XmlNode currencyNode in currencyNodes)
+            {
+                string code = currencyNode.SelectSingleNode("Valute")?.InnerText;
+                decimal rate = decimal.Parse(currencyNode.SelectSingleNode("Value")?.InnerText ?? "0");
+
+                Currency currency = new Currency
+                {
+                    Code = code,
+                    Rate = rate
+                };
+
+                currencies.Add(currency);
+            }
+
+            return currencies;
+        }
+    }
+}
---
+++ ASP/CurrencyConverter/Models/Currency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CurrencyConverter.Models
+{
+    public class Currency
+    {
+        public string Code { get; set; }
+        public string Valute { get; set; }
+        public decimal Rate { get; set; }
+    }
+
+}
+
